168bd6641574d7d32a2e9bd5ea103e316f0975a3
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-10-30 10:52:21 +0100

    Upgrades to freestyle 0.4.1, frees-rpc 0.1.2 (#7)

--- routeguide/src/main/scala/runtime/RouteGuide.scala
+++ routeguide/src/main/scala/runtime/RouteGuide.scala
@@ -17,21 +17,16 @@
 package routeguide
 package runtime
 
-import cats.{~>, Comonad}
+import cats.~>
 import cats.implicits._
 import freestyle.async.implicits._
 import freestyle.rpc.server._
-import journal.Logger
-import monix.eval.Task
 import routeguide.handlers.RouteGuideServiceHandler
 import routeguide.protocols.RouteGuideService
 
-import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
 
 trait RouteGuideEC {
-
-  val logger: Logger = Logger[this.type]
 
   implicit val ec: ExecutionContext = ExecutionContext.Implicits.global
   implicit val S: monix.execution.Scheduler =
@@ -39,41 +34,7 @@ trait RouteGuideEC {
 
 }
 
-trait FutureInstances extends freestyle.rpc.client.FutureInstances {
-
-  protected val atMostDuration: FiniteDuration = 10.seconds
-
-  implicit def futureComonad(implicit ec: ExecutionContext): Comonad[Future] =
-    new Comonad[Future] {
-      def extract[A](x: Future[A]): A = {
-        logger.info(s"${Thread.currentThread().getName} Waiting $atMostDuration for $x...")
-        Await.result(x, atMostDuration)
-      }
-
-      override def coflatMap[A, B](fa: Future[A])(f: (Future[A]) => B): Future[B] = Future(f(fa))
-
-      override def map[A, B](fa: Future[A])(f: (A) => B): Future[B] =
-        fa.map(f)
-    }
-
-  implicit val future2Task: Future ~> Task =
-    new (Future ~> Task) {
-      override def apply[A](fa: Future[A]): Task[A] = {
-        logger.info(s"${Thread.currentThread().getName} Deferring Future to Task...")
-        Task.deferFuture(fa)
-      }
-    }
-}
-
-trait TaskInstances {
-
-  implicit val task2Task: Task ~> Task = new (Task ~> Task) {
-    override def apply[A](fa: Task[A]): Task[A] = fa
-  }
-
-}
-
-trait RouteGuide extends RouteGuideEC with FutureInstances with TaskInstances
+trait RouteGuide extends RouteGuideEC
 
 object server {
 
